9b54f002f4e39cae07870cd6f28733f25d97ef09
puspachauhan98 <puspachauhan01@gmail.com>
2025-04-06 11:31:26 +0545

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,34 @@
+/** @format */
+
+import React from "react";
+
+function Form() {
+  return (
+    <div className="main">
+      <p className="sign" align="center">
+        Sign in
+      </p>
+      <form className="form1">
+        <input
+          className="username"
+          type="text"
+          placeholder="Username"
+          required
+        />
+        <input
+          className="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <a className="submit" align="center">
+          Sign in
+        </a>
+        <p className="forgot" align="center">
+          <a href="#">Forgot Password?</a>
+        </p>
+      </form>
+    </div>
+  );
+}
+export default Form;
